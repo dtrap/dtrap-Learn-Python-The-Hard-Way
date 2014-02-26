de3bcb10c79db0a3022484ab518a38a574877e00
dtrap <dtraphagen@gmail.com>
2014-02-26 15:00:00 -0800

    Going back and doing exercises 1-10 for funsies, though I think my understanding is past this point. I think it's good to understand the author's approach in the basics. Like my man B.K.S. Iyengar says: "A house without firm foundations cannot stand."

---
+++ ex1.py
@@ -0,0 +1,9 @@
+#A good first program
+
+print "Hello World!"
+print "Hello Again"
+print "I like typing this."
+print "This is fun."
+print 'Yay! Printing.'
+print "I'd much rather you say 'not'."
+print 'I "said" do not touch this.'
