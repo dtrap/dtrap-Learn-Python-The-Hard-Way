c63494beefaa2ac66cf382081065e53693af87ee
dtrap <dtraphagen@gmail.com>
2014-03-01 15:34:46 -0800

    Adding ex2 to the mix. Luckily, I already knew what an octothorpe was since I was one for Halloween. That was a pretty slick Halloween costume that I found out had even further cerebral meaning than I thought. I intended it as a cop-out costume. As in, my costume became whomever I was standing next to. Though I already knew you can # to make comments or disable parts of your program...this also added the fun connection that I was actually an invisible program. Maybe I'll rehash (pun intended) this costume next year and be just that. You're welcome planet for this contribution to exercise 2.

---
+++ ex2.py
@@ -0,0 +1,11 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+# But people can read this when they check out your files...
+# So don't be talking about peoples mamas up in here.
+
+print "I could have code like this." # something not about your mama.
+
+# You can also use a comment to "disable" or comment out a piece of code. 
+# print "This won't run."
+
+print "This will run."
